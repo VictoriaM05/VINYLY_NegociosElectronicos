64cdde1ab6ffa743aa886ac6120d9309596ed68b
Victoria Martinez Gutierrez <victoriamartinezgutierrez15@gmail.com>
2026-09-17 21:31:03 -0600

    Agregue imagenes a catalogo

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 
 const productos = [
-  { id: 1,  nombre: "Neón Nocturno",     artista: "Nova Reyes",  precio: "$420 MXN", imagen: "img-1",  descripcion: "Un vinilo cargado de sintetizadores y luces de neón, grabado para sonar tan vibrante en tus bocinas como en la pista de baile." },
+  { id: 1,  nombre: "1989",     artista: "Taylor Swift",  precio: "$820 MXN", imagen: "img-1",  foto: "resources/1989_disco.jpg", descripcion: "..." },
   { id: 2,  nombre: "Corazón de Cristal", artista: "Aria Duval",  precio: "$380 MXN", imagen: "img-2",  descripcion: "Baladas pop delicadas, prensadas en vinilo de 180 gramos para resaltar cada matiz de la voz de Aria." },
   { id: 3,  nombre: "Luces de la Ciudad", artista: "Marco Solis", precio: "$450 MXN", imagen: "img-3",  descripcion: "Edición prensada en vinilo de 180 gramos, con funda gatefold y arte exclusivo. Grabado en estudio analógico para conservar la calidez del sonido original." },
   { id: 4,  nombre: "Eco Dorado",        artista: "Kel Osei",    precio: "$400 MXN", imagen: "img-4",  descripcion: "Ritmos cálidos y coros dorados, masterizados especialmente para vinilo con un rango dinámico más amplio." },
